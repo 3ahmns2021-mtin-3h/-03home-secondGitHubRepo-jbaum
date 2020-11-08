8a4fb72a715b1ec849f293ed25955cacb3550e70
JanaBaumgartner <jbaumgartner0110@icloud.com>
2020-11-08 10:47:28 +0100

    Added Exit Script

---
+++ Assets/MyGame/Scripts/Exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        EditorSceneManager.LoadScene("EndScene");
+    }
+}
